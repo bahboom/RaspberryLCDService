62e283f47c852f7dbf7a0b0895e6f861c04ea0ce
Ming Liu <mliu@Mings-Mac-Pro.home>
2015-04-26 00:44:58 -0400

    adding code

--- scripts/lcd_service.py
+++ scripts/lcd_service.py
@@ -1,1 +1,18 @@
 __author__ = 'mliu'
+
+import RPi.GPIO
+
+
+def init_gpio():
+    print 'Initializing GPIOs...'
+    RPi.GPIO.setmode(RPi.GPIO.BCM)
+
+
+def shutdown():
+    print 'Shutting down...'
+    return
+
+print 'Starting Raspberry LCD Service...'
+init_gpio()
+shutdown()
+print 'Good bye!'
